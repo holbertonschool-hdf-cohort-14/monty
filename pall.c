0c59b79a9e8151aafed82aafedb2a4199731bda9
hollaze <deperiers.a@gmail.com>
2021-05-11 16:37:52 +0200

    trying reverse linked list

--- pall.c
+++ pall.c
@@ -1,16 +1,30 @@
 #include "monty.h"
 
-void _pall(const stack_t *stack)
+void _pall(stack_t *head)
 {
-    if (stack == NULL)
+    stack_t *new_node;
+
+    new_node = malloc(sizeof(stack_t));
+
+    if (new_node == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if (head == NULL)
     {
         fprintf(stderr, "Error: Can't open file <file>");
         exit(EXIT_FAILURE);
     }
 
-    while (stack != NULL)
+    head = new_node;
+    new_node->prev = NULL;
+    new_node->next = head;
+    head->prev = new_node;
+
+    while (head != NULL)
     {
-        printf("%i\n", stack->n);
-        stack = stack-> next; /* print head to tail, need to print tail to head */
+        printf("%i\n", head->n);
+        head = head-> next; /* print head to tail, need to print tail to head */
     }
 }
