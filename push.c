1b1bab6003e39c3747fa2d01873721a363ffd59e
hollaze <deperiers.a@gmail.com>
2021-05-11 16:40:49 +0200

    Need review

--- push.c
+++ push.c
@@ -1,18 +1,18 @@
 #include "monty.h"
 
-void _push(stack_t **stack, int n)
+void _push(stack_t **head, int n)
 {
     stack_t *new_node;
     new_node = malloc(sizeof(stack_t));
     
-    if (new_node == NULL)
+    if (new_node == NULL || *head == NULL)
     {
         fprintf(stderr, "Error: malloc failed\n");
         exit(EXIT_FAILURE);
     }
 
     new_node->n = n;
-    new_node->next = *stack;
-    *stack = new_node;
+    new_node->next = *head;
+    *head = new_node;
 
 }
