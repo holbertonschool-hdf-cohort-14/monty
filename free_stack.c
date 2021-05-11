ba61913c3af979ec156ff5441acb1e9ab0ac0a33
hollaze <deperiers.a@gmail.com>
2021-05-11 21:13:39 +0200

    free function

---
+++ free_stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * free_stack - free memory of the stack
+ * @stack: stack of linked list
+ */
+
+void free_stack(stack_t **stack)
+{
+    stack_t *tmp = *stack;
+
+    while (tmp)
+    {
+        tmp = tmp->next;
+        free(*stack);
+        *stack = tmp;
+    }
+}
