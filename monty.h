724f397750845db07c553e9d55292776ca447307
hollaze <deperiers.a@gmail.com>
2021-05-11 16:42:19 +0200

    Adding push & pall prototypes

--- monty.h
+++ monty.h
@@ -36,4 +36,7 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void _pall(stack_t *head);
+void _push(stack_t **head, int n);
+
 #endif /* MONTY_H */
