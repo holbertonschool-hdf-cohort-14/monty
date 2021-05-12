ae74f5ba38ef133e27136a575ea37fbfa4a8e953
hollaze <deperiers.a@gmail.com>
2021-05-12 11:00:15 +0200

    Fixing betty

--- isdigit.c
+++ isdigit.c
@@ -1,24 +1,24 @@
 #include "monty.h"
 
 /**
- * _isdigit - checks for a digit (0 through 9)
+ * _isdigit - checks if string has a digit
  * @c: variable to check
  * Return: 0 if c is a digit. 1 otherwise
  */
 
 int _isdigit(char *c)
 {
-    int i = 0;
+	int i = 0;
 
-    if (c == NULL)
-        return (1);
+	if (c == NULL)
+		return (1);
 
-    while (c[i])
-    {
-        if (isdigit(c[i]) == 0)
-            return (1);
-        i++;
-    }
+	while (c[i])
+	{
+		if (isdigit(c[i]) == 0)
+			return (1);
+		i++;
+	}
 
 	return (0);
 }
