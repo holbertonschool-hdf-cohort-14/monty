02f3b899572b06befcf97dfc0571c698dfb2673f
SimonBr017 <simonbrard017@gmail.com>
2021-05-12 19:19:09 +0200

    fix swap

--- swap.c
+++ swap.c
@@ -11,8 +11,9 @@ void _swap(stack_t **stack, unsigned int line_number __attribute__((unused)))
 stack_t *tmp, *new_node;
 
 if ((*stack)->next == NULL || *stack == NULL)
+{
 _Error(Error_swap, NULL, line_number);
-
+}
 new_node = *stack;
 tmp = (*stack)->next;
 
