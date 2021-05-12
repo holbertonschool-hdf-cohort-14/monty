e355d710a19fef24ea199b03eb43598c6220a61e
SimonBr017 <simonbrard017@gmail.com>
2021-05-12 19:08:58 +0200

    fix betty add

--- add.c
+++ add.c
@@ -1,13 +1,17 @@
 #include "monty.h"
-
+/**
+ * _add - function tha add opvalue
+ * @stack: stack
+ * @line_number: line number
+ */
 void _add(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-    unsigned int add = 0;
+unsigned int add = 0;
 
-    if ((*stack)->next == NULL || *stack == NULL)
-        _Error(Error_add, NULL, line_number);
+if ((*stack)->next == NULL || *stack == NULL)
+_Error(Error_add, NULL, line_number);
 
-    add = (*stack)->n + (*stack)->next->n;
-    (*stack)->n = add;
-    delete_nodeint_at_index(stack, 1);
+add = (*stack)->n + (*stack)->next->n;
+(*stack)->n = add;
+delete_nodeint_at_index(stack, 1);
 }
