be2b17b41bebab4ea1332a915e83949fbb7d7cbf
SimonBr017 <simonbrard017@gmail.com>
2021-05-12 19:49:33 +0200

    fix betty Error

--- Error.c
+++ Error.c
@@ -1,13 +1,10 @@
 #include "monty.h"
-
 /**
  * _Error - function who print error on stderr
  * @error_num: error number
  * @op_code: operation code
  * @line_number: line number
- * Return: nothing
  */
-
 void _Error(int error_num, char *op_code, unsigned int line_number)
 {
 	switch (error_num)
@@ -16,46 +13,37 @@ void _Error(int error_num, char *op_code, unsigned int line_number)
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 		break;
-
 	case 101:
 		fprintf(stderr, "Error: Can't open file %s\n", op_code);
 		exit(EXIT_FAILURE);
 		break;
-
 	case 102:
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, op_code);
 		exit(EXIT_FAILURE);
 		break;
-
 	case 103:
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 		break;
-
 	case 104:
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 		break;
-
 	case 105:
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 		break;
-
 	case 106:
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 		break;
-
 	case 107:
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 		break;
-	
 	case 108:
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 		break;
 	}
-
 }
