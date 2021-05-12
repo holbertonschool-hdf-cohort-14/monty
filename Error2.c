acc67cb739fa99fae30ff60e71535c576e557ffe
SimonBr017 <simonbrard017@gmail.com>
2021-05-12 21:21:52 +0200

    fix error2

--- Error2.c
+++ Error2.c
@@ -2,10 +2,12 @@
 /**
  * _Error2 - function who print error on stderr
  * @error_num: error number
+ * @op_code: operation code
  * @line_number: line number
  */
-void _Error2(int error_num, unsigned int line_number)
+void _Error2(int error_num, char *op_code, unsigned int line_number)
 {
+(void)op_code;
 	switch (error_num)
 	{
 	case 200:
