3059af283f443487631ebb57ca16b7bd3b79a6f6
hollaze <deperiers.a@gmail.com>
2021-05-12 11:16:07 +0200

    Fixing betty

--- main.c
+++ main.c
@@ -11,43 +11,39 @@ char *op_value = NULL;
 
 int main(int argc, char **argv)
 {
-    FILE *file = NULL;
-    char *line = NULL;
-    size_t line_len = 0;
-    unsigned int line_number = 1;
-    char *op_code = NULL;
-    int handler = 0;
-    stack_t *stack = NULL;
+	FILE *file = NULL;
+	char *line = NULL;
+	size_t line_len = 0;
+	unsigned int line_number = 1;
+	char *op_code = NULL;
+	int handler = 0;
+	stack_t *stack = NULL;
 
-    if (argc != 2)
-    {
-        _Error(Error_input, NULL, 0);
-    }
+	if (argc != 2)
+		_Error(Error_input, NULL, 0);
 
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        _Error(Error_file, argv[1], 0);
-    }
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+		_Error(Error_file, argv[1], 0);
 
-    while (getline(&line, &line_len, file) != -1)
-    {
-        line_number++;
-        op_code = strtok(line, DELIMITEUR);
-        if (op_code)
-        {
-            op_value = strtok(NULL, DELIMITEUR);
-            handler = get_opcode(op_code, &stack, line_number);
-            if (handler == 0)
-            {
-                _Error(Error_instruction, op_code, line_number);
-                free_stack(&stack);
-                fclose(file);
-            }
-        }
-    }
-    free_stack(&stack);
-    free(line);
-    fclose(file);
-    return (0);
+	while (getline(&line, &line_len, file) != -1)
+	{
+		line_number++;
+		op_code = strtok(line, DELIMITEUR);
+		if (op_code)
+		{
+			op_value = strtok(NULL, DELIMITEUR);
+			handler = get_opcode(op_code, &stack, line_number);
+			if (handler == 0)
+			{
+				_Error(Error_instruction, op_code, line_number);
+				free_stack(&stack);
+				fclose(file);
+			}
+		}
+	}
+	free_stack(&stack);
+	free(line);
+	fclose(file);
+	return (0);
 }
--- nop.c
+++ nop.c
@@ -1,7 +1,13 @@
 #include "monty.h"
 
+/**
+ * _nop - do nothing
+ * @stack: stack
+ * @line_number: line number
+ */
+
 void _nop(stack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
