5eb11f70fe6ce1d825ae3280342b8d384cf9a2b0
hollaze <deperiers.a@gmail.com>
2021-05-12 14:07:03 +0200

    Fixing betty

--- swap.c
+++ swap.c
@@ -8,15 +8,15 @@
 
 void _swap(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-    stack_t *tmp, *new_node;
+	stack_t *tmp, *new_node;
 
-    if ((*stack)->next == NULL || *stack == NULL)
-        _Error(Error_swap, NULL, line_number);
+	if ((*stack)->next == NULL || *stack == NULL)
+		_Error(Error_swap, NULL, line_number);
 
-    new_node = *stack;
-    tmp = (*stack)->next;
-    new_node->next = tmp->next;
-    new_node->prev = tmp;
-    tmp->next = new_node;
-    *stack = tmp;
+	new_node = *stack;
+	tmp = (*stack)->next;
+	new_node->next = tmp->next;
+	new_node->prev = tmp;
+	tmp->next = new_node;
+	*stack = tmp;
 }
